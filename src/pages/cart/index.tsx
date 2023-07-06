379e3a18a80aac82d39b8c8446f8a7072dd07962
Julio Junior <julio@Mac-mini-de-Julio.local>
2023-07-06 18:18:14 -0300

    Adicionando componentes de produto, api e carrinho.

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,33 @@
 export function Cart (){
     return (
-        <div>
-            <h1>Carrinho Page</h1>
+        <div className="w-full max-w-7xl mx-auto">
+            <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
+
+            <section className="flex items-center justify-between border-b-2 border-gray-300">
+                <img 
+                    src="https://cdn.awsli.com.br/2500x2500/1417/1417526/produto/195071970/fone-de-ouvido-bluetooth-apple-airpods-3o-geracao-mme73am-a-case-carregamento-magsafe-f1-7df82da845.jpg"
+                    alt="Logo Produto"
+                    className="w-28"
+                />
+
+                <strong>Preço da Unidade: R$ 1.000,00</strong>
+
+                <div className="flex items-center justify-center gap-3">
+                    <button className="bg-slate-600 px-2 rounded font-medium flex items-center justify-center">
+                        -
+                    </button>
+                        2
+                    <button className="bg-slate-600 px-2 rounded font-medium flex items-center justify-center">
+                        +
+                    </button>
+                </div>
+
+                <strong className="float-right">
+                    Subtotal: R$1.000,00
+                </strong>
+            </section>
+
+            <p className="font-bold mt-4">Total: R$1.000,00</p>
         </div>
     )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,46 @@
-export function Home(){
+import { useState, useEffect } from 'react'
+import { api } from '../../services/api'
+import { ProdutoCard } from '../../components/Product'
+
+interface ProductProps {
+    id: number,
+    title: any,
+    description: any,
+    price: any,
+    cover: any
+}
+
+export function Home() {
+
+    const [products, setProducts] = useState<ProductProps[]>([])
+
+    useEffect(() => {
+        async function getProducts() {
+            const response = await api.get('/products')
+            setProducts(response.data)
+            console.log(products)
+        }
+
+        getProducts()
+    }, [])
     return (
-        <>
-            <div>Home!!</div>
-        </>
+        <div>
+            <main className="w-full max-w-7xl px-4 mx-auto">
+                <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em Alta</h1>
+
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5 place-items-end">
+                    {products.map(({title, price, cover}) => {
+                        return (
+                            <>
+                                <ProdutoCard cover={cover} title={title} price={price.toLocaleString("pt-BR", {
+                                    style: "currency",
+                                    currency: "BRL"
+                                })} />
+                            </>
+                        )
+                    })}
+                </div>
+            </main>
+        </div>
     )
 }
