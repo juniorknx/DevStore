595916ba6f49e1953830beef4f1a29ad08f1a3ea
Julio Junior <julio@Mac-mini-de-Julio.local>
2023-07-07 18:15:11 -0300

    Listando carrinho e criando total do carrinho

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,33 +1,56 @@
-export function Cart (){
+import { useContext } from "react"
+import { CartContext } from "../../contexts/CartContext"
+import { Link } from "react-router-dom"
+
+export function Cart() {
+
+    const { cart, total, addItemCart, removeItemCart } = useContext(CartContext)
+
+    console.log('csrt ====>', cart)
+
     return (
         <div className="w-full max-w-7xl mx-auto">
             <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
 
-            <section className="flex items-center justify-between border-b-2 border-gray-300">
-                <img 
-                    src="https://cdn.awsli.com.br/2500x2500/1417/1417526/produto/195071970/fone-de-ouvido-bluetooth-apple-airpods-3o-geracao-mme73am-a-case-carregamento-magsafe-f1-7df82da845.jpg"
-                    alt="Logo Produto"
-                    className="w-28"
-                />
+            {cart.length === 0 && (
+                <div className="flex justify-center flex-col items-center">
+                    <p className="mb-4">Carrinho vazio! :( </p>
 
-                <strong>Preço da Unidade: R$ 1.000,00</strong>
-
-                <div className="flex items-center justify-center gap-3">
-                    <button className="bg-slate-600 px-2 rounded font-medium flex items-center justify-center">
-                        -
-                    </button>
-                        2
-                    <button className="bg-slate-600 px-2 rounded font-medium flex items-center justify-center">
-                        +
-                    </button>
+                    <Link to="/">
+                        <span className="font-bold bg-slate-600 p-2 rounded text-white">Voltar</span>
+                    </Link>
                 </div>
+            )}
 
-                <strong className="float-right">
-                    Subtotal: R$1.000,00
-                </strong>
-            </section>
+            {cart.map((item) => {
+                return (
+                    <section className="flex items-center justify-between border-b-2 border-gray-300">
+                        <img
+                            src={item?.cover}
+                            alt="Logo Produto"
+                            className="w-28"
+                        />
 
-            <p className="font-bold mt-4">Total: R$1.000,00</p>
+                        <strong>Preço da Unidade: {item?.price.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</strong>
+
+                        <div className="flex items-center justify-center gap-3">
+                            <button onClick={() => removeItemCart(item)} className="bg-slate-600 px-2 rounded font-medium flex items-center justify-center">
+                                -
+                            </button>
+                            {item.amount}
+                            <button onClick={() => addItemCart(item)} className="bg-slate-600 px-2 rounded font-medium flex items-center justify-center">
+                                +
+                            </button>
+                        </div>
+
+                        <strong className="float-right">
+                            Subtotal: {item?.total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+                        </strong>
+                    </section>
+                )
+            })}
+
+            {cart.length !== 0 && <p className="font-bold mt-4">Total: {total} </p> }
         </div>
     )
 }
